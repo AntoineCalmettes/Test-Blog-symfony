3a6b0bdff4f9b8e07b6a96795971fa4e4e695ea1
antoinecalmettes <calmettes.antoine2@gmail.com>
2019-04-20 11:24:34 +0200

    First commit

---
+++ Blog/src/Entity/User.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity("email")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     * @Assert\Email
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=10, minMessage="Votre mot de passe doit faire 10 caractéres")
+     * @Assert\EqualTo(propertyPath="confirm_password", message="Votre mot de passe n'est pas pareil")
+     */
+    private $password;
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="Votre mot de passe n'est pas pareil")
+     */
+    public $confirm_password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function eraseCredentials(){
+
+    }
+
+    public function getSalt(){
+
+    }
+
+    public function getRoles(){
+        return ['ROLE_USER'];
+      
+    }
+
+}
